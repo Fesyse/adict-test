e4563ca596b3888a17a90d71d12e7a6eee7ace03
fesyse <maksimlol.mail.com@mail.ru>
2025-03-07 17:12:03 +0800

    setup pagination for posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
 import { PostCard } from "@/components/posts/post-card";
 import { PostCardLoading } from "@/components/posts/post-card-loading";
 import { PostsFilters } from "@/components/posts/posts-filters";
+import { PostsPagination } from "@/components/posts/posts-pagination";
 import { usePosts } from "@/hooks/use-posts";
-import { parseNumberFromSearchParams } from "@/lib/utils";
-import { useSearchParams } from "react-router";
+import { usePostsPaginationStore } from "@/store/posts-pagination.store";
 
 export const PostsPage = () => {
-	const [searchParams] = useSearchParams();
 	const { data: posts, isLoading } = usePosts();
-
-	const page = parseNumberFromSearchParams(searchParams.get("page"), 0);
-	const limit = parseNumberFromSearchParams(searchParams.get("limit"), 10);
+	const { limit, page } = usePostsPaginationStore();
 
 	const paginatedPosts = posts?.slice(page * limit, page * limit + limit);
 
 	return (
 		<div className="min-h-screen py-20 max-w-lg mx-auto">
 			<PostsFilters className="mb-8" />
+
 			<ul className="flex flex-col gap-6">
 				{paginatedPosts?.length && !isLoading ? (
 					paginatedPosts.map((post) => <PostCard key={post.id} post={post} />)
@@ -28,6 +26,8 @@ export const PostsPage = () => {
 					))
 				)}
 			</ul>
+
+			<PostsPagination />
 		</div>
 	);
 };
