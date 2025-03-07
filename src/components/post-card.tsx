1db3869656088a0d6dde5bb394dc9bec282d7ae2
fesyse <maksimlol.mail.com@mail.ru>
2025-03-07 16:16:22 +0800

    feat: add little chevron to post card

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/types/post";
+import { ChevronRight } from "lucide-react";
 import { Link } from "react-router";
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
 
@@ -10,12 +11,16 @@ export const PostCard: React.FC<PostCardProps> = ({ post }) => {
 	return (
 		<li>
 			<Link to={`/posts/${post.id}`}>
-				<Card>
+				<Card className="group">
 					<CardHeader>
 						<CardTitle>{post.title}</CardTitle>
 						<CardDescription>{post.body}</CardDescription>
 
-						<p className="text-foreground/80">Posted by @{post.userId}</p>
+						<div className="flex justify-between gap-2">
+							<p className="text-foreground/80">Posted by @{post.userId}</p>
+
+							<ChevronRight className="h-5 w-5 group-hover:translate-x-1 transition-transform duration-300" />
+						</div>
 					</CardHeader>
 				</Card>
 			</Link>
